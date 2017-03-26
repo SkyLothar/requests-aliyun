8b6a7163c21a6ef3533d19cb16e9b01e5ce282bb
SkyLothar <allothar@gmail.com>
2017-03-26 21:21:57 +0800

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import sys
 
 from codecs import open
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 __author__ = "SkyLothar"
 __email__ = "allothar@gmail.com"
 __url__ = "http://github.com/skylothar/requests-aliyun"
